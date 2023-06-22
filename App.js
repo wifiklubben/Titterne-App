1f430926fe9c5088637961a75e8e8e6638e38f27
Gareth Davies <mrgarethddavies@gmail.com>
2023-06-22 11:28:43 +0200

    add tabs

--- App.js
+++ App.js
@@ -1,19 +1,35 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+ function Empty() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!😍 </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+export default function myTabs() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Empty} />
+        <Tab.Screen name="Settings" component={Empty} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'hotpink',
     alignItems: 'center',
     justifyContent: 'center',
   },
