3d793e68ff7856265886684b15239f7ff17d9ba9
Gareth Davies <mrgarethddavies@gmail.com>
2023-07-11 14:41:24 +0200

    catch up

--- App.js
+++ App.js
@@ -1,30 +1,64 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+// import * as ScreenOrientation from 'expo-screen-orientation';
 
 const Tab = createBottomTabNavigator();
 
- function Empty() {
+
+  
+
+
+ function Empty1() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!😍 </Text>
+      <Image source={
+        {uri: "/assets/"}
+      }
+      style={{
+        height: 200,
+        width: 200,
+        marginBottom: 24,
+      }}
+      ></Image>
+
+      <Text>I'm a kyoot wittle kitty😍 </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+ function Empty2() {
+  return (
+    <View style={styles.container}>
+      <Image source={
+        {uri: "https://media.tenor.com/pZoEd_5z0rMAAAAM/being-silly-hey.gif"}
+      }
+      style={{
+        height: 300,
+        width: 300,
+        marginBottom: 24,
+      }}
+      ></Image>
+
+      <Text>I'm also a kyoot wittle kitty😍 </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-export default function myTabs() {
+export default function myTabs() {  
+ 
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={Empty} />
-        <Tab.Screen name="Settings" component={Empty} />
+        <Tab.Screen name="Page 1" component={Empty1} />
+        <Tab.Screen name="Page 2" component={Empty2} />
+        <Tab.Screen name="Page 3" component={Empty1} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
