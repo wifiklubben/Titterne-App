5d2fdd1c779a2a94564d8dc0daf9d9d4f142b120
Gareth Davies <mrgarethddavies@gmail.com>
2023-10-25 16:20:08 +0200

    improved resolution on animation, needs final fixs

--- BedroomView.js
+++ BedroomView.js
@@ -287,9 +287,9 @@ console.log("story closing");
     <View style={{
         position: 'absolute',
         height: 540,
-        width: 800,
-        top: 140,
-        left: 530,
+        width: 300,
+        top: 240,
+        left: 730,
 
         }}>
           <SpriteSheet 
@@ -299,11 +299,10 @@ console.log("story closing");
               position: 'absolute',
               left: 100,
             }}
-            columns={7}
-            rows={9}
-            width={800}
-            frameWidth={960}
-            frameHeight={540}
+            columns={9}
+            rows={7}
+            width={300}
+            frameWidth={733}
             animations={{
               waveSleep: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62],
               idle: [0],
