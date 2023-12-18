c8fdb8c5142b3dbe8bd693719f0dd4a21a820fe7
Tiril Grimsrud <tiril_k_g@hotmail.com>
2023-12-18 11:51:39 +0100

    inital test particles

--- SockGameView.js
+++ SockGameView.js
@@ -1,348 +1,352 @@
-import React, {useEffect, useState} from 'react'
-import { View, ImageBackground, Image, FlatList, Pressable, Animated, Text } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  ImageBackground,
+  Image,
+  FlatList,
+  Pressable,
+  Animated,
+  Text,
+} from "react-native";
+import { Emitter } from "react-native-particles";
 
-import { Audio } from 'expo-av'
+import { Audio } from "expo-av";
 
 // matching socks stock function
 function areSocksMatched(sock1, sock2) {
+  console.log("sock1 id: ", sock1.id);
+  console.log("sock2 id: ", sock2.id);
 
-    console.log("sock1 id: ", sock1.id);
-    console.log("sock2 id: ", sock2.id);
-
-    if (sock1.sock.color === sock2.sock.color && sock1.id != sock2.id) {
-
-        return(sock1, sock2)
-
-    } else {}
+  if (sock1.sock.color === sock2.sock.color && sock1.id != sock2.id) {
+    return sock1, sock2;
+  } else {
+  }
 }
 
+function SockGameView(props) {
+  const [popSound, setPopSound] = useState();
+  const [fartSound, setFartSound] = useState();
+  const [yaySound, setYaySound] = useState();
 
-function SockGameView( props ) {
+  const [openCards, setOpenCards] = useState([]);
+  const [clearedCards, setClearedcards] = useState([]);
 
-    const [popSound, setPopSound] = useState()
-    const [fartSound, setFartSound] = useState()
-    const [yaySound, setYaySound] = useState()
-
-    const [openCards, setOpenCards] = useState([])
-    const [clearedCards, setClearedcards] = useState([]);
-
-
-    useEffect(() => {
-
-        async function loadPop() {
-            try {
-            const { sound } = await Audio.Sound.createAsync(
-                require('./assets/audio/pop.mp3')
-            );
-            setPopSound( sound )
-            } catch (error) {
-                console.log("error loading sound: ", error);
-            }
-        };
-        loadPop()
-    }, [])
-    
-    useEffect(() => {
-    
-        async function loadFart() {
-            try {
-            const { sound } = await Audio.Sound.createAsync(
-                require('./assets/audio/fart.mp3')
-            );
-            setFartSound( sound )
-            } catch (error) {
-                console.log("error loading sound: ", error);
-            }
-        };
-        loadFart()
-    }, [])
-    
-    useEffect(() => {
-    
-        async function loadYay() {
-            try {
-            const { sound } = await Audio.Sound.createAsync(
-                require('./assets/audio/yay.mp3')
-            );
-            setYaySound( sound )
-            } catch (error) {
-                console.log("error loading sound: ", error);
-            }
-        };
-        loadYay()
-    }, [])
-    
-    const uniqueSocksArray = [
-        {
-            color: "red",
-            image: require('./assets/graphics/socks/sockRed.png')
-        },
-        {
-            color: "blue",
-            image: require('./assets/graphics/socks/sockBlue.png')
-        },
-        {
-            color: 'yellow',
-            image: require('./assets/graphics/socks/sockYellow.png')
-        },
-        {
-            color: 'purple',
-            image: require('./assets/graphics/socks/sockPurple.png')
-        },
-        {
-            color: 'pink',
-            image: require('./assets/graphics/socks/sockPink.png')
-        },
-        {
-            color: 'green',
-            image: require('./assets/graphics/socks/sockGreen.png')
-        }
-    ]
-    
-    
-    // play sounds
-    
-    const playPopSound = () => {
-        try {
-             popSound.stopAsync();
-             popSound.playAsync();
-        } catch(error) {
-            console.log("PLAYBACK error here is: ", error);
-        }
-    }
-    
-    const playFartSound = () => {
-        try {
-             fartSound.stopAsync();
-             fartSound.playAsync();
-        } catch(error) {
-            console.log("PLAYBACK error here is: ", error);
-        }
-    }
-    
-    const playYaySound = () => {
-        try {
-             yaySound.stopAsync();
-             yaySound.playAsync();
-        } catch(error) {
-            console.log("PLAYBACK error here is: ", error);
-        }
-    }
-    
-    
-    
-    //shuffle function
-    
-    function shuffleSocks(array) {
-        const length = array.length;
-        for (let i = length; i > 0; i--) {
-            const randomIndex = Math.floor(Math.random() * i);
-            const currentIndex = i-1;
-            const temp = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temp;
-        }
-        return array
-    }
-    
-     //picking socks
-    
-     const handleCardClick = async (clickedSock, sockId) => {
-    
-        if (openCards.length === 1) {
-            setOpenCards((prev) => [...prev, { sock: clickedSock, id: sockId}]);
-        
-        } else {
-            setOpenCards([{ sock: clickedSock, id: sockId}]);
-        }
-    
-    };
-    
-    
-    // verifying socks
-    
-    useEffect(() => {
-    
-        if (openCards.length === 2) {
-    
-            if(areSocksMatched(openCards[0], openCards[1])) {
-                setClearedcards(prevClearedCards => [...prevClearedCards, ...openCards])
-                playYaySound()
-            } else {
-                playFartSound()
-            }
-        }
-    
-    
-    },[openCards])
-    
-    // checking picked up socks for win condition
-    
-    useEffect(() => {
-    
-        if (clearedCards.length === 12) {
-            console.log("YOU DID IT");
-        
-            const delay = 2000;
-            const timeoutId = setTimeout(() => {
-                //reset game upon win
-                setClearedcards([]);
-                setOpenCards([]);
-                shuffleSocks(uniqueSocksArray);
-            }, delay);
-            return () => clearTimeout(timeoutId);
-    
-        }
-    }, [clearedCards])
-    
-    
-        //Rendering socks
-    
-    const renderItem = ({item, index}) => {
-        const image = item.image
-        const sockId = index
-    
-        const isCardOpen = openCards.some(card => card.id === sockId);
-        const isCardCleared = clearedCards.some(card => card.sock.color === item.color);
-    
-        return (
-            
-            <Pressable onPress={() => [handleCardClick(item, sockId), playPopSound()]}
-            style={{
-                opacity: isCardCleared ? 0 : 1,
-                marginVertical: 0,
-                marginHorizontal: 10,
-            }}
-            >
-            <Image 
-                data={"color"}
-                source = {image}
-                style={{
-                    height: 120,
-                    width: 120,
-                    resizeMode: 'contain',
-                    opacity: 1,
-                    shadowColor: 'white',
-                    shadowOpacity: isCardOpen ? 1 : 0,
-                    shadowRadius: 3,
-                    overflow: 'visible',
-                }}
-                />
-            </Pressable>
-    
-            )
-        }
-    
-        const [cards, setCards] = useState(
-            () => shuffleSocks(uniqueSocksArray.concat(uniqueSocksArray))
+  useEffect(() => {
+    async function loadPop() {
+      try {
+        const { sound } = await Audio.Sound.createAsync(
+          require("./assets/audio/pop.mp3")
         );
+        setPopSound(sound);
+      } catch (error) {
+        console.log("error loading sound: ", error);
+      }
+    }
+    loadPop();
+  }, []);
 
-  return (
-    <View style={{zIndex: 10}}>
-    <ImageBackground 
-    source={ require('./assets/Bedroom/Carpet_sockgame.png')}
-    resizeMode="contain"
-    style={{
-        position: 'absolute',
-        padding: 20,
-        width: 850,
-        height: 850,
-        top: 0,
-        left: 150,
-    }}
-    >
-    <View 
-    justifyContent={'center'}
-    alignItems={'center'}
-    style={{
-        position: 'absolute',
-        padding: 20,
-        width: 600,
-        height: 600,
-        top: 110,
-        left: 110,
-        borderRadius: '25%',
-    }}>
+  useEffect(() => {
+    async function loadFart() {
+      try {
+        const { sound } = await Audio.Sound.createAsync(
+          require("./assets/audio/fart.mp3")
+        );
+        setFartSound(sound);
+      } catch (error) {
+        console.log("error loading sound: ", error);
+      }
+    }
+    loadFart();
+  }, []);
 
-    <Pressable onPress={() => props.handleGameOpen()}
-                        style={{
-                            position: 'absolute',
-                            right: 0,
-                            top: 0,
-                            zIndex: 100,
-                        }}
-                        >
-                        <Image source={require('./assets/Global/closeIcon.png')}
-                            style={{
-                                width: 100,
-                                height: 100,
-                                resizeMode: 'contain',
-                            }}
-                        />  
-        </Pressable>
+  useEffect(() => {
+    async function loadYay() {
+      try {
+        const { sound } = await Audio.Sound.createAsync(
+          require("./assets/audio/yay.mp3")
+        );
+        setYaySound(sound);
+      } catch (error) {
+        console.log("error loading sound: ", error);
+      }
+    }
+    loadYay();
+  }, []);
 
-        {clearedCards.length < 12 &&
-        <FlatList
-            numColumns={4}
-            data={cards}
-            renderItem={renderItem}
-            justifyContent= {'space-around'}
-            alignItems = {'center'}
-            style = {{
-                width: '100%',
-                height: "100%",
+  const uniqueSocksArray = [
+    {
+      color: "red",
+      image: require("./assets/graphics/socks/sockRed.png"),
+    },
+    {
+      color: "blue",
+      image: require("./assets/graphics/socks/sockBlue.png"),
+    },
+    {
+      color: "yellow",
+      image: require("./assets/graphics/socks/sockYellow.png"),
+    },
+    {
+      color: "purple",
+      image: require("./assets/graphics/socks/sockPurple.png"),
+    },
+    {
+      color: "pink",
+      image: require("./assets/graphics/socks/sockPink.png"),
+    },
+    {
+      color: "green",
+      image: require("./assets/graphics/socks/sockGreen.png"),
+    },
+  ];
 
-            }}>
-        </FlatList>}
+  // play sounds
 
-        {clearedCards.length  === 12 &&
-        <Text style={[props.styles.h1Text,
-        ]}>You did it!!</Text>}
-  
+  const playPopSound = () => {
+    try {
+      popSound.stopAsync();
+      popSound.playAsync();
+    } catch (error) {
+      console.log("PLAYBACK error here is: ", error);
+    }
+  };
 
-    </View>
-    </ImageBackground>
+  const playFartSound = () => {
+    try {
+      fartSound.stopAsync();
+      fartSound.playAsync();
+    } catch (error) {
+      console.log("PLAYBACK error here is: ", error);
+    }
+  };
 
-    {/* show different Thorden when half way! */}
+  const playYaySound = () => {
+    try {
+      yaySound.stopAsync();
+      yaySound.playAsync();
+    } catch (error) {
+      console.log("PLAYBACK error here is: ", error);
+    }
+  };
 
-    {clearedCards.length < 6 && <Animated.Image source={require(
-        './assets/Thorden.png'
-        )}
-        style={{
-            position: 'absolute',
-            resizeMode: 'contain',
-            height: '30%',
-            bottom: 0
-        }}>
+  //shuffle function
 
-    </Animated.Image>}
-
-    {clearedCards.length > 5 && clearedCards.length < 10 && <Animated.Image source={require(
-        './assets/ThordenSocks.png'
-        )}
-        style={{
-            position: 'absolute',
-            resizeMode: 'contain',
-            height: '30%',
-            bottom: 0
-        }}>
-
-    </Animated.Image>}
-
-    {clearedCards.length > 9 && <Animated.Image source={require(
-        './assets/ThordenSockPile.png'
-        )}
-        style={{
-            position: 'absolute',
-            resizeMode: 'contain',
-            height: '30%',
-            bottom: 0
-        }}>
-
-    </Animated.Image>}
-    </View>
-  )
-
+  function shuffleSocks(array) {
+    const length = array.length;
+    for (let i = length; i > 0; i--) {
+      const randomIndex = Math.floor(Math.random() * i);
+      const currentIndex = i - 1;
+      const temp = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temp;
+    }
+    return array;
   }
 
-export default SockGameView
+  //picking socks
+
+  const handleCardClick = async (clickedSock, sockId) => {
+    if (openCards.length === 1) {
+      setOpenCards((prev) => [...prev, { sock: clickedSock, id: sockId }]);
+    } else {
+      setOpenCards([{ sock: clickedSock, id: sockId }]);
+    }
+  };
+
+  // verifying socks
+
+  useEffect(() => {
+    if (openCards.length === 2) {
+      if (areSocksMatched(openCards[0], openCards[1])) {
+        setClearedcards((prevClearedCards) => [
+          ...prevClearedCards,
+          ...openCards,
+        ]);
+        playYaySound();
+      } else {
+        playFartSound();
+      }
+    }
+  }, [openCards]);
+
+  // checking picked up socks for win condition
+
+  useEffect(() => {
+    if (clearedCards.length === 12) {
+      //   console.log("YOU DID IT");
+
+      const delay = 10000000;
+      const timeoutId = setTimeout(() => {
+        //reset game upon win
+        setClearedcards([]);
+        setOpenCards([]);
+        shuffleSocks(uniqueSocksArray);
+      }, delay);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [clearedCards]);
+
+  //Rendering socks
+
+  const renderItem = ({ item, index }) => {
+    const image = item.image;
+    const sockId = index;
+
+    const isCardOpen = openCards.some((card) => card.id === sockId);
+    const isCardCleared = clearedCards.some(
+      (card) => card.sock.color === item.color
+    );
+
+    return (
+      <Pressable
+        onPress={() => [handleCardClick(item, sockId), playPopSound()]}
+        style={{
+          opacity: isCardCleared ? 0 : 1,
+          marginVertical: 0,
+          marginHorizontal: 10,
+        }}
+      >
+        <Image
+          data={"color"}
+          source={image}
+          style={{
+            height: 120,
+            width: 120,
+            resizeMode: "contain",
+            opacity: 1,
+            shadowColor: "white",
+            shadowOpacity: isCardOpen ? 1 : 0,
+            shadowRadius: 3,
+            overflow: "visible",
+          }}
+        />
+      </Pressable>
+    );
+  };
+
+  const [cards, setCards] = useState(() =>
+    shuffleSocks(uniqueSocksArray.concat(uniqueSocksArray))
+  );
+
+  return (
+    <View style={{ zIndex: 10 }}>
+      <ImageBackground
+        source={require("./assets/Bedroom/Carpet_sockgame.png")}
+        resizeMode="contain"
+        style={{
+          position: "absolute",
+          padding: 20,
+          width: 850,
+          height: 850,
+          top: 0,
+          left: 150,
+        }}
+      >
+        <View
+          justifyContent={"center"}
+          alignItems={"center"}
+          style={{
+            position: "absolute",
+            padding: 20,
+            width: 600,
+            height: 600,
+            top: 110,
+            left: 110,
+            borderRadius: "25%",
+          }}
+        >
+          <Pressable
+            onPress={() => props.handleGameOpen()}
+            style={{
+              position: "absolute",
+              right: 0,
+              top: 0,
+              zIndex: 100,
+            }}
+          >
+            <Image
+              source={require("./assets/Global/closeIcon.png")}
+              style={{
+                width: 100,
+                height: 100,
+                resizeMode: "contain",
+              }}
+            />
+          </Pressable>
+
+          {clearedCards.length < 12 && (
+            <FlatList
+              numColumns={4}
+              data={cards}
+              renderItem={renderItem}
+              justifyContent={"space-around"}
+              alignItems={"center"}
+              style={{
+                width: "100%",
+                height: "100%",
+              }}
+            ></FlatList>
+          )}
+
+          {clearedCards.length === 12 && (
+            // <Text style={[props.styles.h1Text]}>You did it!!</Text>
+            <Emitter
+              numberOfParticles={20}
+              emissionRate={2}
+              interval={500}
+              particleLife={2000}
+              direction={-200}
+              spread={800}
+              fromPosition={{ x: 300, y: 300 }}
+            >
+              <Image
+                source={require("./assets/graphics/socks/sockBlue.png")}
+                style={{ resizeMode: "contain", height: 80, width: 80 }}
+              />
+            </Emitter>
+          )}
+        </View>
+      </ImageBackground>
+
+      {/* show different Thorden when half way! */}
+
+      {clearedCards.length < 6 && (
+        <Animated.Image
+          source={require("./assets/Thorden.png")}
+          style={{
+            position: "absolute",
+            resizeMode: "contain",
+            height: "30%",
+            bottom: 0,
+          }}
+        ></Animated.Image>
+      )}
+
+      {clearedCards.length > 5 && clearedCards.length < 10 && (
+        <Animated.Image
+          source={require("./assets/ThordenSocks.png")}
+          style={{
+            position: "absolute",
+            resizeMode: "contain",
+            height: "30%",
+            bottom: 0,
+          }}
+        ></Animated.Image>
+      )}
+
+      {clearedCards.length > 9 && (
+        <Animated.Image
+          source={require("./assets/ThordenSockPile.png")}
+          style={{
+            position: "absolute",
+            resizeMode: "contain",
+            height: "30%",
+            bottom: 0,
+          }}
+        ></Animated.Image>
+      )}
+    </View>
+  );
+}
+
+export default SockGameView;
--- TreehouseView.js
+++ TreehouseView.js
@@ -127,7 +127,10 @@ function TreehouseView({ styles }) {
 
   return (
     <>
-      <ImageBackground source={require("./assets/TreeHouse/TreeHouseBG.png")} style={styles.fullWidthBackground}>
+      <ImageBackground
+        source={require("./assets/TreeHouse/TreeHouseBG.png")}
+        style={styles.fullWidthBackground}
+      >
         {bugGameOpen === false && (
           <Pressable
             onPress={() => handleGameOpen()}
@@ -171,7 +174,11 @@ function TreehouseView({ styles }) {
             frameHeight={150}
             frameWidth={150}
             animations={{
-              squawk: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42],
+              squawk: [
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+                18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
+                34, 35, 36, 37, 38, 39, 40, 41, 42,
+              ],
             }}
           ></SpriteSheet>
 
@@ -297,7 +304,12 @@ function TreehouseView({ styles }) {
               zIndex: 0,
             }}
             animations={{
-              wave: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57],
+              wave: [
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+                18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
+                34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+                50, 51, 52, 53, 54, 55, 56, 57,
+              ],
               blink: [0, 1, 2, 3, 4, 5, 6, 7, 8],
             }}
           ></SpriteSheet>
@@ -349,7 +361,9 @@ function TreehouseView({ styles }) {
           />
         </View>
 
-        {bugGameOpen === true && <BugGameView styles={styles} handleGameOpen={handleGameOpen} />}
+        {bugGameOpen === true && (
+          <BugGameView styles={styles} handleGameOpen={handleGameOpen} />
+        )}
 
         {/* story book component
 
